9e0cbd813c7a21dab79183f840576a823b5d15e6
NathaReis <153759333+NathaReis@users.noreply.github.com>
2024-06-27 17:44:57 -0300

    Relógio e Clima visualização

---
+++ clima/main.js
@@ -0,0 +1,61 @@
+const cityName = 'Sete Lagoas';
+const apiKey = 'af771379a3ebcb50459501b069cdebc8';
+
+const getDataCity = async (cityName, apiKey) => {
+
+    const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${encodeURI(cityName)}&appid=${apiKey}`;
+    const results = await fetch(apiUrl);
+    const resultsUrl = results.url;
+
+    const data = await fetch(resultsUrl)
+    .then(response => response.json())
+    .then(data => data)
+    .catch(err => console.error(err));
+
+    return data[0];
+}
+
+const getCurrentClimate = async () => {
+    const dataCity = new City(await getDataCity(cityName, apiKey));
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${dataCity.lat}&lon=${dataCity.lon}&appid=${apiKey}&units=metric&lang=pt_br`;
+    const results = await fetch(apiUrl);
+    const resultsUrl = results.url;
+
+    const data = await fetch(resultsUrl)
+    .then(response => response.json())
+    .then(data => data)
+    .catch(err => console.error(err));
+
+    const information = {data: data, dataCity: dataCity}
+
+    return information;
+}
+
+const render = (information) => {
+    console.log(information)
+    const $image = document.querySelector("#image");
+    const $temp = document.querySelector(".temp");
+    const $desc = document.querySelector(".desc");
+    const $cep = document.querySelector(".cep");
+
+    $image.src = `http://openweathermap.org/img/wn/${information.data.weather[0].icon}@2x.png`;
+    $temp.innerHTML = `${information.data.main.temp} C°`;
+    $desc.innerHTML = String(information.data.weather[0].description).toUpperCase();
+    $cep.innerHTML = `${information.dataCity.name} ${information.dataCity.country}`;
+}
+
+setInterval(() => {
+    getCurrentClimate()
+    .then(render);
+}, 60000*10)
+getCurrentClimate()
+.then(render);
+// CLOCK
+setInterval(() => {
+    const $hour = document.querySelector("#hour");
+    const $min = document.querySelector("#min");
+    const now = new Date();
+    $hour.innerHTML = now.getHours() < 10 ? `0${now.getHours()}` : now.getHours();
+    $min.innerHTML = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
+}, 1000);
---
+++ clima/models/City.js
@@ -0,0 +1,9 @@
+class City {
+    constructor(data) {
+        this.name = data.name,
+        this.lat = data.lat, 
+        this.lon = data.lon, 
+        this.country = data.country, 
+        this.state = data.state
+    }
+}
